0ef0c89352ab6c9a861f7adc6ceba7362783f3aa
Okyfunoky <okyfunoky@hotmail.com>
2019-09-13 12:45:25 -0700

    Fully functioning, unique correct answer generating code

--- assets/scripts/getClues.js
+++ assets/scripts/getClues.js
@@ -2,10 +2,13 @@ var Question = /** @class */ (function () {
     function Question(answer, clue) {
         this.cityName = answer;
         this.clue = clue;
+        this.answer = false;
     }
     return Question;
 }());
 var quests;
+var usedCitys = Array();
+var currentQuestions = Array();
 function buildQuestions(response) {
     var questions = Array();
     response.forEach(function (question) {
@@ -13,6 +16,14 @@ function buildQuestions(response) {
         questions.push(newQuestion);
     });
     return questions;
+}
+function shuffleArray(array) {
+    var _a;
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        _a = [array[j], array[i]], array[i] = _a[0], array[j] = _a[1];
+    }
+    return array;
 }
 function getQuestions() {
     var queryURL = "http://jservice.io/api/clues?category=";
@@ -23,12 +34,31 @@ function getQuestions() {
         method: "GET"
     }).then(function (response) {
         quests = buildQuestions(response);
+        test();
+        return currentQuestions;
     })["catch"](function (err) {
         console.error("Problem getting data from jservice:" + err);
     });
 }
 function parseQuestions(questions) {
-    questions.forEach(function (Question) {
-        console.log(Question.clue);
-    });
+    //redeclare the array to empty it out for new use;
+    currentQuestions = Array();
+    shuffleArray(questions);
+    while (currentQuestions.length < 4) {
+        for (var index = 0; index < 4; index++) {
+            var element = questions[index];
+            if (usedCitys.indexOf(element.cityName) === -1) {
+                currentQuestions.push(element);
+            }
+        }
+    }
+}
+function getAnswer() {
+    currentQuestions = shuffleArray(currentQuestions);
+    currentQuestions[0].answer = true;
+    usedCitys.push(currentQuestions[0].cityName);
+}
+function test() {
+    parseQuestions(quests);
+    getAnswer();
 }
--- assets/scripts/getClues.ts
+++ assets/scripts/getClues.ts
@@ -1,24 +1,35 @@
 class Question {
     cityName: string;
     clue: string;
+    answer: boolean;
 
     constructor(answer: string, clue: string) {
         this.cityName = answer;
         this.clue = clue;
+        this.answer = false;
     }
 }
 
 var quests;
+var usedCitys = Array<string>();
+var currentQuestions = Array<Question>();
 
-
-function buildQuestions(response): Array<Question>{
+function buildQuestions(response): Array<Question> {
     let questions = Array<Question>();
     response.forEach(question => {
         let newQuestion = new Question(question.answer, question.question);
         questions.push(newQuestion);
     });
-    
+
     return questions;
+}
+
+function shuffleArray(array: Array<Question>) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 function getQuestions() {
@@ -31,13 +42,35 @@ function getQuestions() {
         method: "GET"
     }).then(function (response) {
         quests = buildQuestions(response);
+        test();
+        return currentQuestions;
+
     }).catch((err) => {
         console.error("Problem getting data from jservice:" + err);
     });
 }
 
-function parseQuestions(questions: Array<Question>){
-    questions.forEach(Question => {
-        console.log(Question.clue);
-    });
+function parseQuestions(questions: Array<Question>) {
+    //redeclare the array to empty it out for new use;
+    currentQuestions = Array<Question>();
+    shuffleArray(questions);
+    while (currentQuestions.length < 4) {
+        for (let index = 0; index < 4; index++) {
+            const element = questions[index];
+            if (usedCitys.indexOf(element.cityName) === -1) {
+                currentQuestions.push(element);
+            }
+        }
+    }
+}
+
+function getAnswer() {
+    currentQuestions = shuffleArray(currentQuestions);
+    currentQuestions[0].answer = true;
+    usedCitys.push(currentQuestions[0].cityName);
+}
+
+function test() {
+    parseQuestions(quests);
+    getAnswer();
 }
