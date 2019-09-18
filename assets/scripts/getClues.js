69dbe17ab28de31d3c0014a2865d07524abd3d56
Okyfunoky <okyfunoky@hotmail.com>
2019-09-18 13:51:50 -0700

    adding an s

--- assets/scripts/getClues.js
+++ assets/scripts/getClues.js
@@ -36,7 +36,7 @@ function shuffleArray(array) {
     return array;
 }
 function getQuestions() {
-    var queryURL = "http://jservice.io/api/clues?category=";
+    var queryURL = "https://jservice.io/api/clues?category=";
     var category = "78";
     queryURL += category;
     var d = $.Deferred();
